9acad89b2e645dd6ff0f0fa9b867f3cb98fe379a
yichunzhao <zhaoyichun@hotmail.com>
2021-05-25 05:17:31 +0200

    Initial commit

---
+++ src/main/java/com/ynz/demo/UsingPostgresFromDocker.java
@@ -0,0 +1,8 @@
+package com.ynz.demo;
+
+public class UsingPostgresFromDocker {
+
+    public static void main(String[] args) {
+        System.out.println("linking to a Postgres database from a Docker container");
+    }
+}
